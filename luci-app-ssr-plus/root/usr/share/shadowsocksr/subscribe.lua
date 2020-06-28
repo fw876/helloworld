4e82a737b3ca77b3093da01a750e73f7120af6cb
Mattraks <16359027+Mattraks@users.noreply.github.com>
2020-06-28 14:01:48 +0800

    Repair server or port check

--- luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+++ luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
@@ -260,7 +260,9 @@ local function processData(szType, content)
 		result.password = password
 	end
 	if not result.alias then
-		result.alias = result.server .. ':' .. result.server_port
+		if result.server and result.server_port then
+			result.alias = result.server .. ':' .. result.server_port
+		end
 	end
 	-- alias 不参与 hashkey 计算
 	local alias = result.alias
