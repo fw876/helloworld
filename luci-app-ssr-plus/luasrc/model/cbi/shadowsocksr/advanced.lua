e3c290b5f01816faec79ccbdfda710031e11cc8a
FenghenHome <FenghenHome@users.noreply.github.com>
2021-02-07 16:10:22 +0800

    Update advanced.lua

--- luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/advanced.lua
+++ luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/advanced.lua
@@ -48,24 +48,18 @@ o = s:option(Flag, "adblock", translate("Enable adblock"))
 o.rmempty = false
 
 o = s:option(Value, "adblock_url", translate("adblock_url"))
-o:value("https://neodev.team/lite_host_dnsmasq.conf", translate("NEO DEV HOST Lite"))
-o:value("https://neodev.team/host_dnsmasq.conf", translate("NEO DEV HOST Full"))
-o:value("https://anti-ad.net/anti-ad-for-dnsmasq.conf", translate("anti-AD"))
-o.default = "https://neodev.team/lite_host_dnsmasq.conf"
+o:value("https://cdn.jsdelivr.net/gh/FenghenHome/filters@gh-pages/dnsmasq.adblock-domains.conf", translate("FenghenHome adblock list"))
+o.default = "https://cdn.jsdelivr.net/gh/FenghenHome/filters@gh-pages/dnsmasq.adblock-domains.conf"
 o:depends("adblock", "1")
 o.description = translate("Support AdGuardHome and DNSMASQ format list")
 
 o = s:option(Value, "gfwlist_url", translate("gfwlist Update url"))
-o:value("https://cdn.jsdelivr.net/gh/YW5vbnltb3Vz/domain-list-community@release/gfwlist.txt", translate("v2fly/domain-list-community"))
-o:value("https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/gfw.txt", translate("Loyalsoldier/v2ray-rules-dat"))
-o:value("https://cdn.jsdelivr.net/gh/Loukky/gfwlist-by-loukky/gfwlist.txt", translate("Loukky/gfwlist-by-loukky"))
-o:value("https://cdn.jsdelivr.net/gh/gfwlist/gfwlist/gfwlist.txt", translate("gfwlist/gfwlist"))
-o.default = "https://cdn.jsdelivr.net/gh/YW5vbnltb3Vz/domain-list-community@release/gfwlist.txt"
+o:value("https://cdn.jsdelivr.net/gh/FenghenHome/filters@gh-pages/dnsmasq.gfw-domains.conf", translate("FenghenHome gfw list"))
+o.default = "https://cdn.jsdelivr.net/gh/FenghenHome/filters@gh-pages/dnsmasq.gfw-domains.conf"
 
 o = s:option(Value, "chnroute_url", translate("Chnroute Update url"))
-o:value("https://ispip.clang.cn/all_cn.txt", translate("Clang.CN"))
-o:value("https://ispip.clang.cn/all_cn_cidr.txt", translate("Clang.CN.CIDR"))
-o.default = "https://ispip.clang.cn/all_cn.txt"
+o:value("https://cdn.jsdelivr.net/gh/17mon/china_ip_list/china_ip_list.txt", translate("Clang.CN"))
+o.default = "https://cdn.jsdelivr.net/gh/17mon/china_ip_list/china_ip_list.txt"
 
 o = s:option(Value, "nfip_url", translate("nfip_url"))
 o:value("https://cdn.jsdelivr.net/gh/QiuSimons/Netflix_IP/NF_only.txt", translate("Netflix IP Only"))
