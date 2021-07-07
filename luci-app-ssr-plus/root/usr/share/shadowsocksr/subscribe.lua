470d978661c07c2f561cb5c17892593a962ce98a
Tianling Shen <cnsztl@immortalwrt.org>
2021-07-08 02:33:23 +0800

    luci-app-ssr-plus: adapt simple-obfs plugin
    Some proxies providers may use "simple-obfs" instead of "obfs-local",
    make it compatible.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+++ luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
@@ -239,6 +239,10 @@ local function processData(szType, content)
 				else
 					result.plugin = plugin_info
 				end
+				-- 部分机场下发的插件名为 simple-obfs，这里应该改为 obfs-local
+				if result.plugin == "simple-obfs" then
+					result.plugin = "obfs-local"
+				end
 			end
 		else
 			result.server_port = host[2]
@@ -260,8 +264,9 @@ local function processData(szType, content)
 		result.plugin_opts = content.plugin_options
 		result.alias = "[" .. content.airport .. "] " .. content.remarks
 		if checkTabValue(encrypt_methods_ss)[result.encrypt_method_ss] then
-			-- 1202 年了还不支持 SS AEAD 的屑机场
 			result.server = nil
+		elseif result.plugin == "simple-obfs" then
+			result.plugin = "obfs-local"
 		end
 	elseif szType == "trojan" then
 		local idx_sp = 0
