2d520586485c0b56b91e37f3b81f9e316dffd344
Your Name <you@example.com>
2021-09-13 00:32:42 +0800

    fix

--- luci-app-ssr-plus/root/etc/ssrplus/blackipv6.sh
+++ luci-app-ssr-plus/root/etc/ssrplus/blackipv6.sh
@@ -1,7 +1,7 @@
-ip6tables -A zone_lan_forward -j zone_wan_dest_REJECT -d 2400:cb00::/32
-ip6tables -A zone_lan_forward -j zone_wan_dest_REJECT -d 2606:4700::/32
-ip6tables -A zone_lan_forward -j zone_wan_dest_REJECT -d 2803:f800::/32
-ip6tables -A zone_lan_forward -j zone_wan_dest_REJECT -d 2405:b500::/32
-ip6tables -A zone_lan_forward -j zone_wan_dest_REJECT -d 2405:8100::/32
-ip6tables -A zone_lan_forward -j zone_wan_dest_REJECT -d 2a06:98c0::/29
-ip6tables -A zone_lan_forward -j zone_wan_dest_REJECT -d 2c0f:f248::/32
+ip6tables -I zone_lan_forward -j zone_wan_dest_REJECT -d 2400:cb00::/32
+ip6tables -I zone_lan_forward -j zone_wan_dest_REJECT -d 2606:4700::/32
+ip6tables -I zone_lan_forward -j zone_wan_dest_REJECT -d 2803:f800::/32
+ip6tables -I zone_lan_forward -j zone_wan_dest_REJECT -d 2405:b500::/32
+ip6tables -I zone_lan_forward -j zone_wan_dest_REJECT -d 2405:8100::/32
+ip6tables -I zone_lan_forward -j zone_wan_dest_REJECT -d 2a06:98c0::/29
+ip6tables -I zone_lan_forward -j zone_wan_dest_REJECT -d 2c0f:f248::/32
