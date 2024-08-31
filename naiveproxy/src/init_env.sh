616f5da4c8889be0543786a062365b0cff30668f
zxl hhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2024-09-01 00:20:20 +0800

    naiveproxy: update to 128.0.6613.40-1

--- naiveproxy/src/init_env.sh
+++ naiveproxy/src/init_env.sh
@@ -64,6 +64,7 @@ enable_mdns=false
 enable_reporting=false
 include_transport_security_state_preload_list=false
 use_nss_certs=false
+enable_device_bound_sessions=false
 
 enable_backup_ref_ptr_support=false
 enable_dangling_raw_ptr_checks=false
