caef0f01af1bb5b3fe911cd5d895b6226ec1f6d8
zxlhhyccc <zxlhhyccc@gmail.com>
2024-12-17 21:36:41 +0800

    luci-app-ssr-plus: Fix vmess's `RAW` transport protocol configuration and subscribe.

--- luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+++ luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
@@ -172,6 +172,9 @@ local function processData(szType, content)
 		result.v2ray_protocol = 'vmess'
 		result.server = info.add
 		result.server_port = info.port
+		if info.net == "tcp" then
+			info.net = "raw"
+		end
 		result.transport = info.net
 		result.alter_id = info.aid
 		result.vmess_id = info.id
@@ -194,7 +197,7 @@ local function processData(szType, content)
 			result.h2_host = info.host
 			result.h2_path = info.path
 		end
-		if info.net == 'tcp' then
+		if info.net == 'raw' or info.net == 'tcp' then
 			if info.type and info.type ~= "http" then
 				info.type = "none"
 			end
