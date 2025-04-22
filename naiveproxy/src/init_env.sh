b7f38bcae45e76068903c903835fce69e61ebac5
zxlhhyccc <zxlhhyccc@gmail.com>
2025-04-22 20:30:04 +0800

    naiveproxy: Update to 135.0.7049.38-2

--- naiveproxy/src/init_env.sh
+++ naiveproxy/src/init_env.sh
@@ -56,7 +56,6 @@ use_udev=false
 use_aura=false
 use_ozone=false
 use_gio=false
-use_gtk=false
 use_platform_icu_alternatives=true
 use_glib=false
 
@@ -69,10 +68,14 @@ enable_mdns=false
 enable_reporting=false
 include_transport_security_state_preload_list=false
 enable_device_bound_sessions=false
+enable_bracketed_proxy_uris=true
+enable_quic_proxy_support=true
+
 use_nss_certs=false
 
 enable_backup_ref_ptr_support=false
 enable_dangling_raw_ptr_checks=false
+enable_shadow_metadata=false
 
 target_os=\"openwrt\"
 target_cpu=\"${naive_arch}\"
