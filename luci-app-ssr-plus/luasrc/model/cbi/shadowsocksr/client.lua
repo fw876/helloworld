4a0277c45ff046793767f0310915523244b2a16c
coolsnowwolf <31687149+coolsnowwolf@users.noreply.github.com>
2025-05-25 23:22:23 +0800

    luci-app-ssr-plus: add default forward DNS

--- luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/client.lua
+++ luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/client.lua
@@ -151,6 +151,7 @@ o:depends("pdnsd_enable", "2")
 o:depends("pdnsd_enable", "3")
 o.description = translate("Custom DNS Server format as IP:PORT (default: 8.8.4.4:53)")
 o.datatype = "ip4addrport"
+o.default = "8.8.4.4:53"
 
 o = s:option(ListValue, "tunnel_forward_mosdns", translate("Anti-pollution DNS Server"))
 o:value("tcp://8.8.4.4:53,tcp://8.8.8.8:53", translate("Google Public DNS"))
